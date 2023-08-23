e32b3634f80359f71465191864de222fc457c245
alternator0 <konrad.fraczek@protonmail.com>
2023-08-23 16:46:35 +0000

    dodano azure key vault

--- connect.php
+++ connect.php
@@ -1,5 +1,9 @@
 <?php
-$con=mysqli_connect("10.0.0.7:3306","Konrad","Changeme1.","grocerydb");
+require 'vendor/autoload.php';
+$secret = new AzKeyVault\Secret('https://dbkey2147333.vault.azure.net/');
+$secretValue = $secret->getSecret('KluczDoBazy');
+$password = $secretValue->secret;
+$con=mysqli_connect("10.0.0.7:3306","Konrad","$password","grocerydb");
 if(!$con)
 {
 	die("cannot connect to server");
